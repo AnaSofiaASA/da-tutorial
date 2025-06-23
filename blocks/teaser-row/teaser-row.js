0be380b9f68631145e426df3744a9031c2d3199f
AnaSofiaAlmeida <ana.almeida@lisbonnearshore.com>
2025-06-23 16:30:34 +0100

    fix teaser row to account for google drive info to get text size and box correct sizes

--- blocks/teaser-row/teaser-row.js
+++ blocks/teaser-row/teaser-row.js
@@ -1,5 +1,8 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {
+    if (block.className.includes("fifty-fifty")) row.className ="fifty-fifty";
+    if (block.className.includes("forty-sixty")) row.className ="forty-sixty";
+    if (block.className.includes("sixty-forty")) row.className ="sixty-forty";
     const imgoverlay = document.createElement('div');
     imgoverlay.classList.add('image-overlay');
     [...row.children].forEach((div) => {
