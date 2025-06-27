32c9e8fb6fbad202f63b60de2a30e600c7437f6c
AnaSofiaAlmeida <ana.almeida@lisbonnearshore.com>
2025-06-27 14:55:40 +0100

    added helix-config

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -2,7 +2,7 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
- * loads and decorates the footer
+ * loads and decorates the footer 
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
